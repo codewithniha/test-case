f687c3e8b71af61fee802d4398643bf00fcfbf71
codewithniha <nihabatool10@gmail.com>
2026-04-25 15:04:34 +0500

    Fix git author email

--- src/test/java/com/niha/LoginTest.java
+++ src/test/java/com/niha/LoginTest.java
@@ -1,4 +1,5 @@
 package com.niha;
+
 import org.junit.Test;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.*;
@@ -10,30 +11,31 @@ import java.util.concurrent.TimeUnit;
 
 public class LoginTest {
   @Test
-  public void test_login_with_incorrect_credentials(){
+  public void test_login_with_incorrect_credentials() {
 
     ChromeOptions options = new ChromeOptions();
-    // options.addArguments("--headless");
+    options.addArguments("--headless");
     options.addArguments("--no-sandbox");
     options.addArguments("--disable-dev-shm-usage");
+    options.addArguments("--disable-gpu");
 
     WebDriver driver = new ChromeDriver(options);
-        
-    try{
+
+    try {
       driver.manage().timeouts().implicitlyWait(Duration.ofMinutes(1));
       driver.get("http://103.139.122.250:4000/");
 
       driver.findElement(By.id("email")).sendKeys("qasim@malik.com");
-            driver.findElement(By.id("password")).sendKeys("abcdefg");
-            driver.findElement(By.id("m_login_signin_submit")).click();
-            WebElement error = driver.findElement(By.xpath("//form//div[1]"));
+      driver.findElement(By.id("password")).sendKeys("abcdefg");
+      driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/div/form/button")).click();
+      WebElement error = driver.findElement(By.xpath("//form//div[1]"));
 
-            String errorText = error.getText();
+      String errorText = error.getText();
 
-            assertTrue(errorText.contains("Incorrect email or password"));
+      assertTrue(errorText.contains("Incorrect email or password"));
 
-        } finally {
-            driver.quit();
+    } finally {
+      driver.quit();
     }
   }
 
