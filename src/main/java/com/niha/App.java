f50fc94f0659c328747b9c24515aaeff4484ebb4
codewithniha <nihabatool10@gmail.com>
2026-04-25 12:55:01 +0500

    adding test file

---
+++ src/main/java/com/niha/App.java
@@ -0,0 +1,13 @@
+package com.niha;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
