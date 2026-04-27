363f14b5bbf15ca0b553ed428bc55c43c0773121
codewithniha <nihabatool10@gmail.com>
2026-04-27 23:40:33 +0500

    Update LoginTest.java

--- src/test/java/com/niha/LoginTest.java
+++ src/test/java/com/niha/LoginTest.java
@@ -2,49 +2,56 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import java.time.Duration;
 import org.junit.Test;
 import static org.junit.Assert.assertTrue;
 
 public class LoginTest {
-
   @Test
   public void test_login_with_incorrect_credentials() {
-
-    // // Tell Selenium where ChromeDriver is located on your computer
-    // System.setProperty("webdriver.chrome.driver",
-    // "C:\\ChromeDriver\\chromedriver.exe");
-
     // Set options for Chrome browser
     ChromeOptions options = new ChromeOptions();
     options.addArguments("--headless"); // no visible window
     options.addArguments("--no-sandbox");
     options.addArguments("--disable-dev-shm-usage");
-
+    
     // Launch Chrome browser
     WebDriver driver = new ChromeDriver(options);
-
-    // Open the login page
-    driver.navigate().to("http://103.139.122.250:4000/");
-
-    // Enter wrong email and password
-    driver.findElement(By.name("email")).sendKeys("qasim@malik.com");
-    driver.findElement(By.name("password")).sendKeys("abcdefg");
-
-    // Click the login button
-    driver.findElement(By.id("m_login_signin_submit")).click();
-
-    // Wait 5 seconds for the page to respond
-    driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-
-    // Find the error message on the page
-    String errorText = driver.findElement(By.xpath(
-        "/html/body/div/div/div[1]/div/div/div/div[2]/form/div[1]")).getText();
-
-    // Check that the error message contains the expected text
-    assertTrue("Error message not found", errorText.contains("Incorrect email or password"));
-
-    // Close the browser
-    driver.quit();
+    
+    try {
+        // Open the login page
+        driver.navigate().to("http://103.139.122.250:4000/");
+        
+        // Create explicit wait
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        
+        // Wait for email field to be present and visible
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.name("email")));
+        
+        // Enter wrong email and password
+        driver.findElement(By.name("email")).sendKeys("qasim@malik.com");
+        driver.findElement(By.name("password")).sendKeys("abcdefg");
+        
+        // Click the login button
+        driver.findElement(By.id("m_login_signin_submit")).click();
+        
+        // Wait for the error message to appear
+        wait.until(ExpectedConditions.presenceOfElementLocated(
+            By.xpath("/html/body/div/div/div[1]/div/div/div/div[2]/form/div[1]")
+        ));
+        
+        // Find the error message on the page
+        String errorText = driver.findElement(By.xpath(
+            "/html/body/div/div/div[1]/div/div/div/div[2]/form/div[1]")).getText();
+        
+        // Check that the error message contains the expected text
+        assertTrue("Error message not found", errorText.contains("Incorrect email or password"));
+        
+    } finally {
+        // Close the browser (this ensures it closes even if test fails)
+        driver.quit();
+    }
   }
 }
