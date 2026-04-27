cc43b4f8b0b230991216285d10f33ff2b580c6a2
codewithniha <nihabatool10@gmail.com>
2026-04-27 20:25:40 +0500

    Update LoginTest.java

--- src/test/java/com/niha/LoginTest.java
+++ src/test/java/com/niha/LoginTest.java
@@ -1,52 +1,48 @@
-package com.niha;
-
-import org.junit.Test;
-import org.openqa.selenium.*;
-import org.openqa.selenium.chrome.*;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import static org.junit.Assert.assertTrue;
-
-import java.time.Duration;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import java.util.concurrent.TimeUnit;
 
 public class LoginTest {
+
   @Test
   public void test_login_with_incorrect_credentials() {
 
-    System.setProperty("webdriver.chrome.verboseLogging", "true");
-System.setProperty("webdriver.chrome.logfile", "/tmp/chromedriver.log");
+    // // Tell Selenium where ChromeDriver is located on your computer
+    // System.setProperty("webdriver.chrome.driver",
+    // "C:\\ChromeDriver\\chromedriver.exe");
+
+    // Set options for Chrome browser
     ChromeOptions options = new ChromeOptions();
-    options.addArguments("--headless");
+    options.addArguments("--headless"); // no visible window
     options.addArguments("--no-sandbox");
     options.addArguments("--disable-dev-shm-usage");
 
-    options.addArguments("--disable-gpu");  
-    options.addArguments("--window-size=1920,1080");
-
-
+    // Launch Chrome browser
     WebDriver driver = new ChromeDriver(options);
 
-    try {
-      driver.manage().timeouts().implicitlyWait(Duration.ofMinutes(1));
-      driver.get("http://103.139.122.250:4000/");
+    // Open the login page
+    driver.navigate().to("http://103.139.122.250:4000/");
 
-      driver.findElement(By.id("email")).sendKeys("qasim@malik.com");
+    // Enter wrong email and password
+    driver.findElement(By.name("email")).sendKeys("qasim@malik.com");
+    driver.findElement(By.name("password")).sendKeys("abcdefg");
 
-            driver.findElement(By.id("password")).sendKeys("abcdefg");
-           driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/div/form/button")).click();
-            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    // Click the login button
+    driver.findElement(By.id("m_login_signin_submit")).click();
 
-      WebElement error = driver.findElement(By.xpath("//form//div[1]"));
+    // Wait 5 seconds for the page to respond
+    driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 
-      String errorText = error.getText();
+    // Find the error message on the page
+    String errorText = driver.findElement(By.xpath(
+        "/html/body/div/div/div[1]/div/div/div/div[2]/form/div[1]")).getText();
 
+    // Check that the error message contains the expected text
+    assert (errorText.contains("Incorrect email or password"));
 
-            assertTrue(errorText.contains("Failed to fetch"));
-
-
-    } finally {
-      driver.quit();
-    }
+    // Close the browser
+    driver.quit();
   }
-
 }
